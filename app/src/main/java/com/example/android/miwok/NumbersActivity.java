260e1f2cc77bf8977dd665ea7a396f4b3848675b
joshua <nirmaljoshua@bpatech.com>
2016-10-23 23:01:47 +0530

    Implemented the ListView for Numbers Activity and also used ArrayList - 10_23

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,12 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -27,23 +32,33 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words=new String[10];
-        words[0]="One";
-        words[1]="Two";
-        words[2]="Three";
-        words[3]="Four";
-        words[4]="Five";
-        words[5]="Six";
-        words[6]="Seven";
-        words[7]="Eight";
-        words[8]="Nine";
-        words[9]="Ten";
-        String temp="";
-        for (int i=0;i<=words.length-1;i++){
-            temp=temp + "\n" +words[i];
+        ArrayList<String> words=new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+       /* String temp="";
+        for (int i=0;i<=words.size()-1;i++){
+            LinearLayout wordsLinear =(LinearLayout) findViewById(R.id.numbersLinearView);
+            TextView wordsTextView=new TextView(this);
+            wordsTextView.setText(words.get(i));
+            wordsTextView.setGravity(1);
+            wordsLinear.addView(wordsTextView);
+           // temp=temp + "\n" +words.get(i);
+            Log.v("wordPositions","The word in the"+i+"th position is"+words.get(i));
         }
-        TextView numbers= (TextView)findViewById(R.id.NumbersWorsListView);
-       numbers.setText(temp);
+       // TextView numbers= (TextView)findViewById(R.id.NumbersWorsListView);
+       //numbers.setText(temp);
+       */
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
+        ListView listView =(ListView)findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
 
     }
 }
