d1e789a3e740ccebf7e2d0f1735fd9946ed1cfd6
joshua <nirmaljoshua@bpatech.com>
2017-05-03 00:25:37 +0530

    Custom Adapter for Recycle list view

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -45,7 +45,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("Ten","ma'aacha"));
 
 
-        WordAdapter<Word> itemAdapter = new WordAdapter(this, R.layout.list_item, words);
+        WordAdapter itemAdapter = new WordAdapter(this, words);
         ListView listView =(ListView)findViewById(R.id.list);
         listView.setAdapter(itemAdapter);
 
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,19 +1,42 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Nirmal Joshua Laptop on 5/1/2017.
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(Context context, List<Word> words) {
+        super(context, 0, words);
+    }
+
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View list
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextview = (TextView) listItemView.findViewById(R.id.list_item_miwokText);
+        miwokTextview.setText(currentWord.getmMiwokWord());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.list_item_DefaultText);
+        defaultTextView.setText(currentWord.getDefaultWord());
+        return listItemView;
+
+
     }
 }
